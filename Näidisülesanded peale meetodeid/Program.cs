2ed0ffff4a82af93a530b389a957cb5f07c64041
Estlib <75094683+Estlib@users.noreply.github.com>
2026-02-20 15:40:09 +0200

    meetodite taaskasutamine

--- Näidisülesanded peale meetodeid/Program.cs
+++ Näidisülesanded peale meetodeid/Program.cs
@@ -4,48 +4,84 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Palun sisesta esimene arv");
-            float arv1 = 0.0f;
-            arv1 = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Palun sisesta esimene arv");
+            //float arv1 = 0.0f;
+            //arv1 = float.Parse(Console.ReadLine());
 
-            Console.WriteLine("Palun sisesta teine arv");
-            float arv2 = 0.0f;
-            arv2 = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Palun sisesta teine arv");
+            //float arv2 = 0.0f;
+            //arv2 = float.Parse(Console.ReadLine());
 
-            Console.WriteLine("Mis tüüpi tehet sa teha tahad? (valik: + - * / ^ V)");
-            string tehteTüüp = "";
-            tehteTüüp = Console.ReadLine();
+            //Console.WriteLine("Mis tüüpi tehet sa teha tahad? (valik: + - * / ^ V)");
+            //string tehteTüüp = "";
+            //tehteTüüp = Console.ReadLine();
 
-            double tehteTulemus = 0.0d;
+            //double tehteTulemus = 0.0d;
 
-            if (tehteTüüp != "")
+            //if (tehteTüüp != "")
+            //{
+            //    switch (tehteTüüp)
+            //    {
+            //        default:
+            //            Console.WriteLine("Tehtetüüp pole valitud, tehet ei sooritata.");
+            //            return;
+            //        case "+":
+            //            tehteTulemus = Liitmine(arv1, arv2);
+            //            break;
+            //        case "-":
+            //            tehteTulemus = Lahutamine(arv1, arv2);
+            //            break;
+            //        case "/":
+            //            tehteTulemus = Jagamine(arv1, arv2);
+            //            break;
+            //        case "*":
+            //            tehteTulemus = Korrutamine(arv1 , arv2);
+            //            break;
+            //        case "^":
+            //            tehteTulemus = Astendamine(arv1,arv2);
+            //            break;
+            //        case "V":
+            //            tehteTulemus = Juurimine(arv1,arv2);
+            //            break;
+            //    }
+            //    Console.WriteLine($"Tehte tulemus on: {arv1} {tehteTüüp} {arv2} = {tehteTulemus}");
+            //}
+            Console.WriteLine("Sisesta oma nimi");
+            string kasutajanimi = GetAnswer();
+            Console.WriteLine("Tere "+kasutajanimi);
+            Console.WriteLine("Palun sisesta ka oma tänavanimi");
+            string tänavanimi = GetAnswer();
+            Console.WriteLine(kasutajanimi+ " ahha! elad "+tänavanimi+" tänaval");
+            IsPensionär();
+            //küsi kasutajalt tema vanust, meetod selgitab välja kas ta on pensionär või mitte, ning meetod ise kuvab sõnumi vastavalt kas on või ei;
+            // kui on - ütle "Kuidas jalad liiguvad? hästi?"
+            // kui ei ole - ütle "Mitu monsterit päevas jood?"
+        }
+
+        private static void IsPensionär()
+        {
+            Console.WriteLine("Kui vana oled?");
+            string vanus = GetAnswer();
+            int arvunavanus = int.Parse(vanus);
+            if (arvunavanus >= 65)
             {
-                switch (tehteTüüp)
-                {
-                    default:
-                        Console.WriteLine("Tehtetüüp pole valitud, tehet ei sooritata.");
-                        return;
-                    case "+":
-                        tehteTulemus = Liitmine(arv1, arv2);
-                        break;
-                    case "-":
-                        tehteTulemus = Lahutamine(arv1, arv2);
-                        break;
-                    case "/":
-                        tehteTulemus = Jagamine(arv1, arv2);
-                        break;
-                    case "*":
-                        tehteTulemus = Korrutamine(arv1 , arv2);
-                        break;
-                    case "^":
-                        tehteTulemus = Astendamine(arv1,arv2);
-                        break;
-                    case "V":
-                        tehteTulemus = Juurimine(arv1,arv2);
-                        break;
-                }
-                Console.WriteLine($"Tehte tulemus on: {arv1} {tehteTüüp} {arv2} = {tehteTulemus}");
+                Console.WriteLine("Kuidas jalad liiguvad? hästi?");
             }
+            else 
+            {
+                Console.WriteLine("Mitu monsterit päevas jood?");
+            }
+        }
+
+        private static string GetAnswer()
+        {
+            string input = "";
+            do
+            {
+                Console.WriteLine("Palun sisesta:");
+                input = Console.ReadLine();
+            } while (input == "");
+            return input;
         }
 
         private static double Juurimine(float arv1, float arv2)
